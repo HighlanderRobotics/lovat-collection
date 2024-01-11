956ea3c372174b9966cd4e74233f9043fa2916d5
MangoSwirl <68089814+MangoSwirl@users.noreply.github.com>
2024-01-11 21:38:45 +0000

    Refactor landscape lock

--- app/game.tsx
+++ app/game.tsx
@@ -1,8 +1,19 @@
+import { useEffect } from 'react';
 import { Game } from '../lib/collection/ui/Game';
-import { useLandscapeLock } from '../lib/collection/ui/useLandscapeLock';
+import { OrientationLock, lockAsync } from 'expo-screen-orientation';
 
 export default function GamePage() {
-    useLandscapeLock();
+    useEffect(() => {
+        (async () => {
+            await lockAsync(OrientationLock.LANDSCAPE);
+        })();
+
+        return () => {
+            (async () => {
+                await lockAsync(OrientationLock.PORTRAIT_UP);
+            })();
+        };
+    }, []);
 
     return (
         <Game />
