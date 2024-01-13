d525a80149bb4ae59e4ea0cebe4ab875d531eca1
MangoSwirl <collin.camerono@gmail.com>
2024-01-13 00:18:00 -0800

    Create separate file for `AutoCollectPieceActions`

--- lib/collection/ui/Game.tsx
+++ lib/collection/ui/Game.tsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { useAtom } from 'jotai';
-import { hasNote, remainingGroundNoteLocationsAtom, reportStateAtom, useAddEvent, useSetPhase } from '../reportStateAtom';
+import { hasNote, reportStateAtom, useAddEvent, useSetPhase } from '../reportStateAtom';
 import { router } from 'expo-router';
 import { PreMatchActions } from './actions/PreMatchActions';
 import { GameViewTemplate } from './GameViewTemplate';
@@ -9,13 +9,8 @@ import { Checkbox } from '../../components/Checkbox';
 import { MatchEventType } from '../MatchEventType';
 import { HasNoteActions } from './actions/HasNoteActions';
 import { ExitWingAction } from './actions/ExitWingAction';
-import { MatchEventPosition, groundNotePositions } from '../MatchEventPosition';
-import { FieldElement } from './FieldElement';
-import { GameAction } from './GameAction';
-import { fieldHeight, fieldWidth } from '../../components/FieldImage';
-import { View } from 'react-native';
-import { colors } from '../../colors';
 import * as Haptics from 'expo-haptics';
+import { AutoCollectPieceActions } from './actions/AutoCollectPieceActions';
 
 export function Game() {
     const [reportState, setReportState] = useAtom(reportStateAtom);
@@ -134,64 +129,3 @@ export function Game() {
             field={<></>} />
     );
 }
-
-const AutoCollectPieceActions = () => {
-    const radius = 35;
-
-    const addEvent = useAddEvent();
-    const [remainingNotes] = useAtom(remainingGroundNoteLocationsAtom);
-
-    return (
-        <>
-            {Object.entries(groundNotePositions).map(([key, position]) => {
-                if (remainingNotes?.includes(key as MatchEventPosition)) {
-                    return (
-                        <FieldElement
-                            key={key}
-                            edgeInsets={[
-                                position.fieldCoordinates.y - (radius / fieldHeight),
-                                1 - position.fieldCoordinates.x - (radius / fieldWidth),
-                                1 - position.fieldCoordinates.y - (radius / fieldHeight),
-                                position.fieldCoordinates.x - (radius / fieldWidth),
-                            ]}
-                        >
-                            <GameAction
-                                color="#C1C337"
-                                borderRadius={100}
-                                icon="upload"
-                                onPress={() => {
-                                    addEvent({
-                                        type: MatchEventType.PickupNote,
-                                        position: key as MatchEventPosition,
-                                    });
-                                }}
-                            />
-                        </FieldElement>
-                    )
-                } else {
-                    <FieldElement
-                        key={key}
-                        edgeInsets={[
-                            position.fieldCoordinates.y - (radius / fieldHeight),
-                            1 - position.fieldCoordinates.x - (radius / fieldWidth),
-                            1 - position.fieldCoordinates.y - (radius / fieldHeight),
-                            position.fieldCoordinates.x - (radius / fieldWidth),
-                        ]}
-                    >
-                        <View
-                            style={{
-                                height: "100%",
-                                width: "100%",
-                                borderRadius: 100,
-                                backgroundColor: colors.onBackground.default,
-                                opacity: 0.1,
-                            }}
-                        />
-                    </FieldElement>
-                }
-            })}
-        </>
-    );
-};
-
-
