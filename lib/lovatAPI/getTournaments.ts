c6dc4076e235a2e89a07810c7514f00adf56c059
MangoSwirl <collin.camerono@gmail.com>
2024-01-19 23:35:24 -0800

    Update endpoint name to match versioned API

--- lib/lovatAPI/getTournaments.ts
+++ lib/lovatAPI/getTournaments.ts
@@ -6,7 +6,7 @@ import { DataSource, LocalCache } from "../localCache";
 export const getTournaments = async () => {
     const scouter = await getScouter();
 
-    const response = await get(`/manager/scouters/${scouter.uuid}/tournaments`);
+    const response = await get(`/v1/manager/scouters/${scouter.uuid}/tournaments`);
 
     if (!response.ok) {
         throw new Error("Error fetching tournaments");
