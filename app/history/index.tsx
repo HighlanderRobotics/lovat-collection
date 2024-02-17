7990566ddfdef1a406a121449689bb5502fd09b5
MangoSwirl <collin.camerono@gmail.com>
2024-02-17 11:40:38 -0800

    Revert weird page transition animation for history on Android

--- app/history/index.tsx
+++ app/history/index.tsx
@@ -19,9 +19,6 @@ export default function History() {
 
     return (
         <>
-            <Stack.Screen options={{
-                animation: 'slide_from_left',
-            }} />
             <NavBar
                 title="History"
                 left={
