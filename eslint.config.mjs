f8ed6db5d2d4022ae2de3e8c2c146732ce56aacd
MangoSwirl <collin.camerono@gmail.com>
2024-08-05 23:58:48 -0700

    Add eslint

---
+++ eslint.config.mjs
@@ -0,0 +1,11 @@
+import pluginJs from "@eslint/js";
+import tseslint from "typescript-eslint";
+import pluginReact from "eslint-plugin-react";
+
+
+export default [
+  {files: ["**/*.{js,mjs,cjs,ts,jsx,tsx}"]},
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  pluginReact.configs.flat.recommended,
+];
