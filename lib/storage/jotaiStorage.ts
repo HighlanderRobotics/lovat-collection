8070c517ac6cff3107e253fd324353e6664d1066
Clouch1234 <143346546+Clouch1234@users.noreply.github.com>
2024-12-08 14:09:33 -0800

    fixed problems with storage - history broken

--- lib/storage/jotaiStorage.ts
+++ lib/storage/jotaiStorage.ts
@@ -5,14 +5,18 @@ import z, { ZodType } from "zod";
 export const createStorage = <Value extends ZodType>(schema: Value) =>
   createJSONStorage<z.infer<Value>>(() => ({
     getItem: async (key: string) => {
-      const value = await AsyncStorage.getItem(key);
-      if (value === null) return undefined;
-      return schema.parse(JSON.parse(value));
+        const value = await AsyncStorage.getItem(key);
+
+        if (value === null) return undefined;
+        console.log({key}, JSON.parse(value));
+        console.log(typeof JSON.parse(value));
+        
+        return schema.parse(JSON.parse(value));
     },
     setItem: async (key: string, value: z.infer<Value>) => {
-      await AsyncStorage.setItem(key, JSON.stringify(value));
+        await AsyncStorage.setItem(key, value);
     },
     removeItem: async (key: string) => {
-      await AsyncStorage.removeItem(key);
+        await AsyncStorage.removeItem(key);
     },
   }));
