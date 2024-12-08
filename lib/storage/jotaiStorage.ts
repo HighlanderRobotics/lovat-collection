36279c073e7ba9f109ccd54351a6219938c1ee99
Clouch1234 <143346546+Clouch1234@users.noreply.github.com>
2024-12-08 15:19:31 -0800

    fix formatting

--- lib/storage/jotaiStorage.ts
+++ lib/storage/jotaiStorage.ts
@@ -5,14 +5,14 @@ import z, { ZodType } from "zod";
 export const createStorage = <Value extends ZodType>(schema: Value) =>
   createJSONStorage<z.infer<Value>>(() => ({
     getItem: async (key: string) => {
-        const value = await AsyncStorage.getItem(key);
-        if (value === null) return null;
-        return JSON.stringify(schema.parse(JSON.parse(value)));
+      const value = await AsyncStorage.getItem(key);
+      if (value === null) return null;
+      return JSON.stringify(schema.parse(JSON.parse(value)));
     },
     setItem: async (key: string, value: z.infer<Value>) => {
-        await AsyncStorage.setItem(key, value);
+      await AsyncStorage.setItem(key, value);
     },
     removeItem: async (key: string) => {
-        await AsyncStorage.removeItem(key);
+      await AsyncStorage.removeItem(key);
     },
   }));
