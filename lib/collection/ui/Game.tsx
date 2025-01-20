0ff4211f08eb45fcf4ad7f6364b3371a3e376a95
Clouch1234 <143346546+Clouch1234@users.noreply.github.com>
2025-01-19 16:09:05 -0800

    Update Game.tsx
    Co-authored-by: MangoSwirl <68089814+MangoSwirl@users.noreply.github.com>

--- lib/collection/ui/Game.tsx
+++ lib/collection/ui/Game.tsx
@@ -264,7 +264,7 @@ export function Game() {
   } as const;
 
   const gameState: GameState = (() => {
-    return gameStates.testing;
+    // return gameStates.testing;
     const hasCoral = reportState.getHasCoral();
     const hasAlgae = reportState.getHasAlgae();
     const hasExited = reportState.getHasExited();
