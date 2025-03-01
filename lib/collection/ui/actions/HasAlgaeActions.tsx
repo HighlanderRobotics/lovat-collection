e6f440940f631e7f8317059bd7c1f5390f51b9dd
alouch1234 <143346546+Clouch1234@users.noreply.github.com>
2025-02-28 16:18:05 -0800

    fix-location

--- lib/collection/ui/actions/HasAlgaeActions.tsx
+++ lib/collection/ui/actions/HasAlgaeActions.tsx
@@ -4,11 +4,15 @@ import { MatchEventType } from "../../MatchEventType";
 import { FieldElement } from "../FieldElement";
 import { GameAction } from "../GameAction";
 import { OverlayState } from "../GameViewTemplate";
+import { AllianceColor } from "../../../models/AllianceColor";
 
 export const HasAlgaeActions = (props: {
   setOverlay: (value: OverlayState) => void;
 }) => {
   const addEvent = useReportStateStore((state) => state.addEvent);
+  const allianceColor = useReportStateStore(
+    (state) => state.meta?.allianceColor,
+  );
   return (
     <>
       <FieldElement edgeInsets={[0, 0.435, 0, 0.435]}>
@@ -19,17 +23,29 @@ export const HasAlgaeActions = (props: {
           }}
         ></GameAction>
       </FieldElement>
-
-      <FieldElement edgeInsets={[0, 0.59, 0.8, 0.275]}>
-        <GameAction
-          color="#9cff9a"
-          onPress={() => {
-            addEvent({
-              type: MatchEventType.ScoreProcessor,
-            });
-          }}
-        ></GameAction>
-      </FieldElement>
+      {allianceColor! === AllianceColor.Red ? (
+        <FieldElement edgeInsets={[0, 0.59, 0.8, 0.275]}>
+          <GameAction
+            color="#9cff9a"
+            onPress={() => {
+              addEvent({
+                type: MatchEventType.ScoreProcessor,
+              });
+            }}
+          ></GameAction>
+        </FieldElement>
+      ) : (
+        <FieldElement edgeInsets={[0.8, 0.59, 0, 0.275]}>
+          <GameAction
+            color="#9cff9a"
+            onPress={() => {
+              addEvent({
+                type: MatchEventType.ScoreProcessor,
+              });
+            }}
+          ></GameAction>
+        </FieldElement>
+      )}
     </>
   );
 };
