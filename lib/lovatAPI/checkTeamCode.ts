9261c8b0732e6f13d6dc3c6a2b6a825e6e628f1e
leok18 <178525594+leok18@users.noreply.github.com>
2025-08-28 17:37:36 -0700

    jstris

--- lib/lovatAPI/checkTeamCode.ts
+++ lib/lovatAPI/checkTeamCode.ts
@@ -1,10 +1,13 @@
 import { z } from "zod";
 import { get } from "./lovatAPI";
 
+import {secret2} from './getTournaments';
+export const secret1 = secret2;
 export const checkTeamCode = async (code: string) => {
   const response = await get(
     `/v1/manager/scouter/checkcode?code=${encodeURIComponent(code)}`,
   );
+
 
   if (!response.ok) {
     throw new Error(response.statusText);
