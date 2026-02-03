94a132ea209ba326e0658f63ff6844a0e46ceab2
MangoSwirl <collin.camerono@gmail.com>
2026-02-02 22:11:10 -0800

    Implement defense button (fixes LVT-132)

--- lib/collection/ui/actions/DefendAction.tsx
+++ lib/collection/ui/actions/DefendAction.tsx
@@ -3,8 +3,15 @@ import { figmaDimensionsToFieldInsets } from "../../util";
 import { FieldElement } from "../FieldElement";
 import { colors } from "../../../colors";
 import { Icon } from "../../../components/Icon";
+import { MatchEventType } from "../../MatchEventType";
+import { useReportStateStore } from "../../reportStateStore";
+import { useState } from "react";
+import * as Haptics from "expo-haptics";
 
 export function DefendAction() {
+  const reportState = useReportStateStore();
+  const [isDefending, setIsDefending] = useState(false);
+
   return (
     <FieldElement
       edgeInsets={figmaDimensionsToFieldInsets({
@@ -15,14 +22,31 @@ export function DefendAction() {
       })}
     >
       <TouchableOpacity
+        onPressIn={() => {
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+          reportState.addEvent({
+            type: MatchEventType.StartDefending,
+          });
+          setIsDefending(true);
+        }}
+        onPressOut={() => {
+          Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+          reportState.addEvent({
+            type: MatchEventType.StopDefending,
+          });
+          setIsDefending(false);
+        }}
         style={{
           width: "100%",
           height: "100%",
           justifyContent: "center",
           alignItems: "center",
           borderRadius: 7,
-          backgroundColor: colors.secondaryContainer.default,
+          backgroundColor: isDefending
+            ? colors.danger.default
+            : colors.secondaryContainer.default,
         }}
+        activeOpacity={1}
       >
         <Icon name="shield" color={colors.onBackground.default} size={48} />
       </TouchableOpacity>
