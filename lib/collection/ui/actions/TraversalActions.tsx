dc69e0d997d9ba33473004be7d94f3730c458023
MangoSwirl <collin.camerono@gmail.com>
2026-02-07 23:05:20 -0800

    Add traversal arrows to buttons indicating robot movement direction

--- lib/collection/ui/actions/TraversalActions.tsx
+++ lib/collection/ui/actions/TraversalActions.tsx
@@ -1,13 +1,56 @@
+import { View } from "react-native";
 import { colors } from "../../../colors";
+import { AllianceColor } from "../../../models/AllianceColor";
+import {
+  FieldOrientation,
+  useFieldOrientationStore,
+} from "../../../storage/userStores";
 import { MatchEventPosition } from "../../MatchEventPosition";
 import { MatchEventType } from "../../MatchEventType";
 import { useReportStateStore } from "../../reportStateStore";
 import { figmaDimensionsToFieldInsets } from "../../util";
 import { FieldElement } from "../FieldElement";
 import { GameAction } from "../GameAction";
+import { Icon } from "../../../components/Icon";
+
+function TraversalArrow() {
+  const fieldOrientation = useFieldOrientationStore((state) => state.value);
+  const allianceColor = useReportStateStore(
+    (state) => state.meta?.allianceColor,
+  );
+  const events = useReportStateStore((state) => state.events);
+
+  const crossCount = events.filter(
+    (event) => event.type === MatchEventType.Cross,
+  ).length;
+  const isInAllianceZone = crossCount % 2 === 0;
+
+  const allianceIsOnLeft =
+    (allianceColor === AllianceColor.Red) ===
+    (fieldOrientation === FieldOrientation.Sinister);
+
+  const arrowPointsLeft = isInAllianceZone
+    ? !allianceIsOnLeft // away from alliance
+    : allianceIsOnLeft; // toward alliance
+
+  return (
+    <View
+      style={{
+        transform: [{ scaleX: arrowPointsLeft ? -1 : 1 }],
+      }}
+    >
+      <Icon
+        name="chevron_right"
+        color={colors.victoryPurple.default}
+        size={32}
+      />
+    </View>
+  );
+}
 
 export default function TraversalActions() {
   const reportState = useReportStateStore();
+
   return (
     <>
       <FieldElement
@@ -26,7 +69,9 @@ export default function TraversalActions() {
             })
           }
           color={colors.victoryPurple.default}
-        />
+        >
+          <TraversalArrow />
+        </GameAction>
       </FieldElement>
       <FieldElement
         edgeInsets={figmaDimensionsToFieldInsets({
@@ -44,7 +89,9 @@ export default function TraversalActions() {
             })
           }
           color={colors.victoryPurple.default}
-        />
+        >
+          <TraversalArrow />
+        </GameAction>
       </FieldElement>
       <FieldElement
         edgeInsets={figmaDimensionsToFieldInsets({
@@ -62,7 +109,9 @@ export default function TraversalActions() {
             })
           }
           color={colors.victoryPurple.default}
-        />
+        >
+          <TraversalArrow />
+        </GameAction>
       </FieldElement>
       <FieldElement
         edgeInsets={figmaDimensionsToFieldInsets({
@@ -80,7 +129,9 @@ export default function TraversalActions() {
             })
           }
           color={colors.victoryPurple.default}
-        />
+        >
+          <TraversalArrow />
+        </GameAction>
       </FieldElement>
     </>
   );
